b657692f1f003b5c059adab552c6aa6e376b76ea
Krishanu Karmakar <krishanu@kali>
2023-12-23 23:57:53 +0530

    multiplayer

--- main.py
+++ main.py
@@ -1,4 +1,118 @@
 import random
+import datetime
+
+def score2(P1_NAME, P1_ATTEMPTS, P2_NAME, P2_ATTEMPTS):
+    P1_SCORE = 10 - P1_ATTEMPTS
+    P2_SCORE = 10 - P2_ATTEMPTS
+
+    if P1_SCORE < 0:
+        P1_SCORE = 0
+    if P2_SCORE < 0:
+        P2_SCORE = 0
+
+    F = open("score2.txt", "r")
+    c = F.read()
+    if c == "" or c == "No Record":
+        q = open("score2.txt", "w")
+        q.write(f"[[{P1_NAME},{P1_SCORE}],[{P2_NAME},{P2_SCORE}]],")
+        q.close()
+    else:
+        q = open("score2.txt", "a")
+        q.write(f"[[{P1_NAME},{P1_SCORE}],[{P2_NAME},{P2_SCORE}]],")
+        q.close()
+    F.close()
+
+    if P1_SCORE == P2_SCORE:
+        print(f"Game Tied with score of each {P1_SCORE}")
+
+    else:
+        if P1_SCORE > P2_SCORE:
+            print(f"{P1_NAME} won with a score of {P1_SCORE} and {P2_NAME} got defeated with score of {P2_SCORE}")
+        else:
+            print(f"{P2_NAME} won with a score of {P2_SCORE} and {P1_NAME} got defeated with score of {P1_SCORE}")
+
+def double_player_game():
+    print("Welcome to the Double Player Guess Game")
+    p1_name = input("Enter Player 1's name: ")
+    p2_name = input("Enter Player 2's name: ")
+
+    p1_secret_number = random.randint(1, 100)
+    p2_secret_number = random.randint(1, 100)
+
+    p1_attempts = 0
+    p2_attempts = 0
+
+    print(f"{p1_name}'s Game Has Started...")
+
+    while True:
+        p1_attempts += 1
+        if p1_attempts < 11:
+            try:
+                guess = int(input("Enter your guess (between 1 and 100): "))
+            except:
+                print("Please enter a integer between 1 and 100")
+                continue
+            if guess == p1_secret_number:
+                print(f"Congratulations! You guessed the correct number in {p1_attempts} attempts.")
+                break
+            elif guess < p1_secret_number:
+                if p1_secret_number - guess > 20:
+                    print("Try a very high number.")
+
+                elif p1_secret_number - guess > 10:
+                    print("Try higher number")
+                else:
+                    print("Just a little more, you almost got it...")
+                
+            else:
+                if p1_secret_number - guess < -20:
+                    print("Try a very small number.")
+
+                elif p1_secret_number - guess < -10:
+                    print("Try smaller number")
+                else:
+                    print("Just a little less, you almost got it...")
+                
+        else:
+            print("You ran out of chances.")
+            break
+
+    print(f"{p2_name}'s Game Has Started")
+
+    while True:
+        p2_attempts += 1
+        if p2_attempts < 11:
+            try:
+                guess = int(input("Enter your guess (between 1 and 100): "))
+            except:
+                print("Please enter a integer between 1 and 100")
+                continue
+            if guess == p2_secret_number:
+                print(f"Congratulations! You guessed the correct number in {p2_attempts} attempts.")
+                break
+            elif guess < p2_secret_number:
+                if p2_secret_number - guess > 20:
+                    print("Try a very high number.")
+
+                elif p2_secret_number - guess > 10:
+                    print("Try higher number")
+                else:
+                    print("Just a little more, you almost got it...")
+                
+            else:
+                if p2_secret_number - guess < -20:
+                    print("Try a very small number.")
+
+                elif p2_secret_number - guess < -10:
+                    print("Try smaller number")
+                else:
+                    print("Just a little less, you almost got it...")
+                
+        else:
+            print("You ran out of chances.")
+            break
+    
+    score2(p1_name, p1_attempts, p2_name, p2_attempts)
 
 
 def single_player_game():
@@ -45,10 +159,20 @@ def score(attempts):
     sc = 10 - attempts
     if sc < 0:
         sc = 0
+    
+    F = open('score.txt', 'r')
+    c = F.read()
+    if c == "" or c == "No Record":
+        f = open("score.txt", 'w')
+        f.write(f'[\'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\',\'{sc}\'];')
+        f.close()
+        
+    else:
+        f = open("score.txt", "a")
+        f.write(f'[\'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\',\'{sc}\'];')
+        f.close()
 
-    f = open('score.txt', 'a')
-    f.write(f'{sc},')
-    f.close()
+    F.close()
     return sc
 
 
@@ -57,7 +181,7 @@ def main():
 
     while True:
         print("\nSelect an option:")
-        print("1. Single Player")
+        print("1. Play Game")
         print("2. Display Score Log")
         print("3. Display High Score")
         print("4. Reset Score")
@@ -66,16 +190,39 @@ def main():
         choice = input("Enter your choice (1, 2, or 3): ")
 
         if choice == "1":
-            single_player_game()
+            u = input("1. Single Player\n2. Multiplayer")
+            if u == '1':
+                single_player_game()
+            elif u == '2':
+                double_player_game()
+            else:
+                print("Wrong Input...")
+                break
 
         elif choice == "2":
-            f = open("score.txt", "r")
-            content = f.read().split(',')[:-1]
-            if content:
-                print("\n".join(content))
+
+            u = input("1. Single Player\n2. Multiplayer")
+            if u == '1':
+                f = open("score.txt", "r")
+                content = f.read().split(';')[:-1]
+                if content:
+                    for rec in content:
+                        rec = eval(rec)
+                        print(f"{rec[0]} ---> {rec[1]}")
+                else:
+                    print("No Record")
+                f.close()
+                
+            elif u == '2':
+                f = open("score2.txt", "r")
+                content = f.read().split(",")[:-1]
+                if content:
+                    pass
             else:
-                print("No Record")
-            f.close()
+                print("Wrong Input...")
+                break
+
+            
 
         elif choice == "3":
             f = open("score.txt", "r")
