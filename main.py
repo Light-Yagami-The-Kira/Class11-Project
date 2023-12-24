d319bdd22cdb511a62160aea5dcd36cee6db26b5
Krishanu Karmakar <krishanu@kali>
2023-12-24 09:37:02 +0530

    guessloop

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 import random
 import datetime
 
+def guess_loop():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    while True:
+        attempts += 1
+        if attempts < 11:
+            try:
+                guess = int(input("Enter your guess (between 1 and 100): "))
+            except:
+                print("Please enter a integer between 1 and 100")
+                continue
+            if guess == secret_number:
+                print(f"Congratulations! You guessed the correct number in {attempts} attempts.")
+                break
+            elif guess < secret_number:
+                if secret_number - guess > 20:
+                    print("Try a very high number.")
+
+                elif secret_number - guess > 10:
+                    print("Try higher number")
+                else:
+                    print("Just a little more, you almost got it...")
+                
+            else:
+                if secret_number - guess < -20:
+                    print("Try a very small number.")
+
+                elif secret_number - guess < -10:
+                    print("Try smaller number")
+                else:
+                    print("Just a little less, you almost got it...")
+                
+        else:
+            print("You ran out of chances.")
+            break
+    return attempts
+
 def score2(P1_NAME, P1_ATTEMPTS, P2_NAME, P2_ATTEMPTS):
     P1_SCORE = 10 - P1_ATTEMPTS
     P2_SCORE = 10 - P2_ATTEMPTS
@@ -36,123 +73,21 @@ def double_player_game():
     p1_name = input("Enter Player 1's name: ")
     p2_name = input("Enter Player 2's name: ")
 
-    p1_secret_number = random.randint(1, 100)
-    p2_secret_number = random.randint(1, 100)
-
-    p1_attempts = 0
-    p2_attempts = 0
-
     print(f"{p1_name}'s Game Has Started...")
 
-    while True:
-        p1_attempts += 1
-        if p1_attempts < 11:
-            try:
-                guess = int(input("Enter your guess (between 1 and 100): "))
-            except:
-                print("Please enter a integer between 1 and 100")
-                continue
-            if guess == p1_secret_number:
-                print(f"Congratulations! You guessed the correct number in {p1_attempts} attempts.")
-                break
-            elif guess < p1_secret_number:
-                if p1_secret_number - guess > 20:
-                    print("Try a very high number.")
-
-                elif p1_secret_number - guess > 10:
-                    print("Try higher number")
-                else:
-                    print("Just a little more, you almost got it...")
-                
-            else:
-                if p1_secret_number - guess < -20:
-                    print("Try a very small number.")
-
-                elif p1_secret_number - guess < -10:
-                    print("Try smaller number")
-                else:
-                    print("Just a little less, you almost got it...")
-                
-        else:
-            print("You ran out of chances.")
-            break
+    p1_attempts = guess_loop()
 
     print(f"{p2_name}'s Game Has Started")
 
-    while True:
-        p2_attempts += 1
-        if p2_attempts < 11:
-            try:
-                guess = int(input("Enter your guess (between 1 and 100): "))
-            except:
-                print("Please enter a integer between 1 and 100")
-                continue
-            if guess == p2_secret_number:
-                print(f"Congratulations! You guessed the correct number in {p2_attempts} attempts.")
-                break
-            elif guess < p2_secret_number:
-                if p2_secret_number - guess > 20:
-                    print("Try a very high number.")
-
-                elif p2_secret_number - guess > 10:
-                    print("Try higher number")
-                else:
-                    print("Just a little more, you almost got it...")
-                
-            else:
-                if p2_secret_number - guess < -20:
-                    print("Try a very small number.")
-
-                elif p2_secret_number - guess < -10:
-                    print("Try smaller number")
-                else:
-                    print("Just a little less, you almost got it...")
-                
-        else:
-            print("You ran out of chances.")
-            break
+    p2_attempts = guess_loop()
     
     score2(p1_name, p1_attempts, p2_name, p2_attempts)
 
 
 def single_player_game():
     print("Welcome to the Single Player Guess Game!")
-    secret_number = random.randint(1, 100)
-
-    attempts = 0
-    while True:
-        attempts += 1
-        if attempts < 11:
-            try:
-                guess = int(input("Enter your guess (between 1 and 100): "))
-            except:
-                print("Please enter a integer between 1 and 100")
-                continue
-            if guess == secret_number:
-                print(f"Congratulations! You guessed the correct number in {attempts} attempts.")
-                break
-            elif guess < secret_number:
-                if secret_number - guess > 20:
-                    print("Try a very high number.")
-
-                elif secret_number - guess > 10:
-                    print("Try higher number")
-                else:
-                    print("Just a little more, you almost got it...")
-                
-            else:
-                if secret_number - guess < -20:
-                    print("Try a very small number.")
-
-                elif secret_number - guess < -10:
-                    print("Try smaller number")
-                else:
-                    print("Just a little less, you almost got it...")
-                
-        else:
-            print("You ran out of chances.")
-            break
-    print(f"Your score is {score(attempts)}")
+    a_a = guess_loop()
+    print(f"Your score is {score(a_a)}")
 
 
 def score(attempts):
@@ -190,7 +125,7 @@ def main():
         choice = input("Enter your choice (1, 2, or 3): ")
 
         if choice == "1":
-            u = input("1. Single Player\n2. Multiplayer")
+            u = input("1. Single Player\n2. Multiplayer: ")
             if u == '1':
                 single_player_game()
             elif u == '2':
@@ -201,7 +136,7 @@ def main():
 
         elif choice == "2":
 
-            u = input("1. Single Player\n2. Multiplayer")
+            u = input("1. Single Player\n2. Multiplayer: ")
             if u == '1':
                 f = open("score.txt", "r")
                 content = f.read().split(';')[:-1]
@@ -232,7 +167,7 @@ def main():
                 continue
 
         elif choice == "3":
-            u = input("1. Single Player\n2. Multiplayer")
+            u = input("1. Single Player\n2. Multiplayer: ")
             if u == '1':
                 f = open("score.txt", "r")
                 content = f.read().split(';')[:-1]
@@ -272,7 +207,7 @@ def main():
 
         elif choice == "4":
             file = None
-            U = input("1. Single Player Score 2. Multiplayer Score")
+            U = input("1. Single Player Score 2. Multiplayer Score: ")
 
             if U == '1':
                 file = 'score.txt'
